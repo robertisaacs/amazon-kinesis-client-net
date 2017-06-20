9ab81509e951e1ca337cc5c3d6ee7de0dbe76842
risaacs <risaacs@connectwise.com>
2017-06-20 11:45:27 -0400

    [WIP] Support .net core, update csproj files, upgrade to .net core compatible dependencies

--- ClientLibrary/Properties/AssemblyInfo.cs
+++ ClientLibrary/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿/*using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
@@ -28,3 +28,4 @@ using System.Runtime.CompilerServices;
 
 // Required for Substitutes in tests
 [assembly:InternalsVisibleTo("DynamicProxyGenAssembly2")]
+*/
