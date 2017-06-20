22eecc037d7fea73be6045cb5325a19c07395f5a
risaacs <risaacs@connectwise.com>
2017-06-20 11:47:28 -0400

    [WIP] Remove test assemblies, remove temporarily AssemblyInfo (replace later with csproj properties)

--- Bootstrap/Properties/AssemblyInfo.cs
+++ Bootstrap/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+﻿/*using System.Reflection;
 
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
@@ -25,3 +25,4 @@
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+*/
